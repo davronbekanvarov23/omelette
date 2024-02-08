24dcb38b8c584dd9f935cb6e161caf19353b7d13
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-02-08 10:21:31 +0500

    complated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: { backgroundColor: { main_pink: "#F3E5D7" } },
+    fontFamily: {
+      sans: ["Outfit", "sans-serif"],
+      serif: ["Young Serif", "serif"],
+    },
+    extend: {
+      backgroundColor: { main_pink: "#F3E5D7" },
+      colors: {
+        "wenge-brown": "#5F564D",
+        snow: "#FFF7FB",
+        "dark-rasbarry": "#7A284E",
+        "brandy-red": "#854632",
+        "dark-charcoal": "#312E2C",
+      },
+    },
   },
   plugins: [],
 };
